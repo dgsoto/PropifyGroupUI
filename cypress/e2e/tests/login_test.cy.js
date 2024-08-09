e04ed47efee1ab7841383441598ae32cc28dddc3
dgsoto <dgsotoqatesting@gmail.com>
2024-08-09 16:54:40 -0400

    created login tests

--- cypress/e2e/tests/login_test.cy.js
+++ cypress/e2e/tests/login_test.cy.js
@@ -1,3 +1,4 @@
+import { LoginPageConstants } from "../pages/propifygroup/auth/login/login-page.constants";
 import { LoginPageHelper } from "../pages/propifygroup/auth/login/login-page.helper";
 import { LoginPageValidation } from "../pages/propifygroup/auth/login/login-page.validation";
 import { CommonPageHelper } from "../pages/propifygroup/common/common-page.helper";
@@ -5,11 +6,25 @@ import { CommonPageValidation } from "../pages/propifygroup/common/common-page.v
 
 describe("Login Suite Tests", () => {
 
-    it("Click in login option", () => {
+    let username;
+    let password;
+
+    before(() => {
+        username = LoginPageConstants.data.username;
+        password = LoginPageConstants.data.password;
+    })
+
+    beforeEach(() => {
         CommonPageHelper.navigateToApplication();
         CommonPageHelper.clickOnLoginOption();
         CommonPageValidation.validateMessage("Iniciar sesión");
+    });
 
+    it("Verify that it is possible to login with valid username and password.", () => {
+        LoginPageHelper.login(username, password);
+    });
+
+    it("Verify that it is not possible to login with empty username and password.", () => {  
         LoginPageHelper.clickOnLoginButton();
         LoginPageValidation.validateLoginErrorMessages();
     });
